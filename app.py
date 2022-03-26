5794d692db75ddd2846d03c98323732cd4a7b3e8
Jorge Castro <nikey_es@yahoo.es>
2022-03-27 00:17:29 +0100

    New Model and layout

--- app.py
+++ app.py
@@ -1,33 +1,22 @@
 import mlflow
 import numpy as np
 import streamlit as st
-from efficientnet.tfkeras import center_crop_and_resize, preprocess_input
 from PIL import Image
-from scipy import misc
 from skimage import io
-from tensorflow import keras
 
 
 def get_model():
-    # logged_model = 'models:/efficientnetv2-s/24'
-    # Load model as a PyFuncModel.
-    model = mlflow.pyfunc.load_model('model_downloaded/model')
+    model = mlflow.pyfunc.load_model('model_downloaded')
     return model
 
-
-# Dimensiones de las imagenes de entrada
-image_size = 384
 
 # Clases
 classes = ['bathroom', 'bedroom', 'dinning', 'frontal', 'kitchen', 'livingroom']
 
 
 # Se recibe la imagen y el modelo, devuelve la predicción
-def model_prediction(img, model):
-
-    x = center_crop_and_resize(img, image_size=image_size)
-    x = preprocess_input(x)
-    x = np.expand_dims(x, 0)
+def model_prediction(image, model):
+    x = np.expand_dims(image, axis=0)
 
     y = model.predict(x)
     return y
@@ -45,15 +34,15 @@ def main():
 
     st.title("Clasificador de habitaciones")
 
-    col1, col2 = st.columns(2)
+    col1, col2, col3 = st.columns(3)
 
-    with col1:
+    with col2:
         url = st.text_input('Pon la URL de una imagen')
         if url:
             image = io.imread(url)
             st.image(image, caption="Imagen", use_column_width=False)
 
-    with col2:
+    with col3:
         img_file_buffer = st.file_uploader("Carga una imagen")
         # El usuario carga una imagen
         if img_file_buffer is not None:
@@ -62,13 +51,14 @@ def main():
             st.header("Imagen Cargada")
             st.image(image, use_column_width=True, width=10)
 
-    # El botón predicción se usa para iniciar el procesamiento
-    if st.sidebar.button("Predecir..."):
-        prediction = model_prediction(image, model)
-        classes_predictions = dict(zip(classes, prediction[0]))
-        sorted_tuples = sorted(classes_predictions.items(), key=lambda item: item[1], reverse=True)
-        st.sidebar.info(f'Predicciones: {sorted_tuples}')
-        st.sidebar.success(f'Mejor Classe: {classes[np.argmax(prediction)]}')
+    with col1:
+        # El botón predicción se usa para iniciar el procesamiento
+        if st.button("Predecir..."):
+            prediction = model_prediction(image, model)
+            classes_predictions = dict(zip(classes, prediction[0]))
+            sorted_tuples = sorted(classes_predictions.items(), key=lambda item: item[1], reverse=True)
+            st.info(f'Predicciones: {sorted_tuples}')
+            st.success(f'Mejor Classe: {classes[np.argmax(prediction)]}')
 
 
 if __name__ == '__main__':
